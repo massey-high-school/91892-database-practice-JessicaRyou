7c6b1edeacbcbe2f53a841e08c63ddbb9a86dd95
JessicaRyou <51727167+JessicaRyou@users.noreply.github.com>
2020-08-11 11:39:46 +1200

    config, topbit, bottombit, index
    fixed config and added topbit bottom bit

---
+++ Game_site/config.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_errors', 'On'); // change to 'Off' when live 
+
+define ("DB_NAME","ryouj72410");
+define ("DB_USERNAME","_ryouj72410");
+define ("DB_PASSWORD","d0a35618c3a2dd7ed65a486a5835d500"); 
+define ("DB_HOST", "localhost");
+
+?>
